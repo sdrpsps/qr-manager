3d1fd48a67fb68d482d822394dcab9fcf1840095
sunny <sunny@bytespark.app>
2025-07-07 13:12:35 +0800

    chore: r2 bucket cache

--- src/lib/storage/index.ts
+++ src/lib/storage/index.ts
@@ -4,9 +4,9 @@ import { cache } from "react";
 export const createStorage = cache(async () => {
   const { env } = await getCloudflareContext({ async: true });
 
-  if (!env.R2) {
+  if (!env.NEXT_INC_CACHE_R2_BUCKET) {
     throw new Error("Storage service not available");
   }
 
-  return env.R2;
+  return env.NEXT_INC_CACHE_R2_BUCKET;
 });
